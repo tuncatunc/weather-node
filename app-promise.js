75679b613cc5c11154863145a307926bcdd25bc5
Tunca Tunç <Tunca Tun>
2016-11-30 01:15:41 +0300

    Added summary information.

--- app-promise.js
+++ app-promise.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+const darkSkyApiKey = '96fef7c5cb15e5d542ac68f3142f935a';
+
 var argv = require('yargs')
     .options({
         a:{
@@ -12,9 +14,8 @@ var argv = require('yargs')
     .help()
     .argv;
 
-const darkSkyApiKey = '96fef7c5cb15e5d542ac68f3142f935a';
-
 var encodedAddress = encodeURIComponent(argv.address);
+
 var geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 axios.get(geocodeUrl)
     .then((response) => {
@@ -24,14 +25,16 @@ axios.get(geocodeUrl)
         var lat = response.data.results[0].geometry.location.lat;
         var lng = response.data.results[0].geometry.location.lng;
         console.log(response.data.results[0].formatted_address);
-        var weatherUrl = `https://api.darksky.net/forecast/${darkSkyApiKey}/${lat},${lng}?lang=tr&units=auto`;
+        var weatherUrl = `https://api.darksky.net/forecast/${darkSkyApiKey}/${lat},${lng}?units=auto`;
         
         return axios.get(weatherUrl);
     })
     .then((response) => {
         var temp = response.data.currently.temperature;
         var tempApparent = response.data.currently.apparentTemperature;
-        console.log(`It is currently ${temp}, feels like ${tempApparent}`);
+        var summary = response.data.currently.summary;
+
+        console.log(`It is currently ${temp}, feels like ${tempApparent}. ${summary}`);
     })
     .catch((e) => {
         if (e.code === 'ENOTFOUND') console.error('Unable to connect to API servers!');
