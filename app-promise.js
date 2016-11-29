0a0792a849ca2a7c050b212322814a8f76de88af
tunca.tunc <tunca.tunc@pro-line.com.tr>
2016-11-29 18:07:52 +0300

    implemented get weather data usin axiom.

--- app-promise.js
+++ app-promise.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 var argv = require('yargs')
     .options({
         a:{
@@ -10,27 +12,26 @@ var argv = require('yargs')
     .help()
     .argv;
 
-const geocode = require('./geocode/geocode.js');
-const weather = require('./weather/weather.js');
+const darkSkyApiKey = '96fef7c5cb15e5d542ac68f3142f935a';
 
-geocode.getGeocode(argv.address, (errorMessage, results) => {
-    if (errorMessage)
-        console.log(JSON.stringify(errorMessage));
-    else{
-        console.log(JSON.stringify(results));
-    }
-});
-
-geocode.getLocation(argv.address, (errorMessage, results) => {
-    if (errorMessage)
-        console.log(JSON.stringify(errorMessage));
-    else{
-        weather.getCurrentWeather(results, (error, current) => {
-            if (error)
-                console.log(error);
-            else
-                console.log(`Current Temperature is ${current.temperature}, it feels like ${current.apparentTemperature}`);
-        })
-    }
-});
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+axios.get(geocodeUrl)
+    .then((response) => {
+        if (response.data.status === 'ZERO_RESULTS'){
+            throw new Error('Unable to find the address.');
+        }
+        var lat = response.data.results[0].geometry.location.lat;
+        var lng = response.data.results[0].geometry.location.lng;
+        console.log(response.data.results[0].formatted_address);
+        var weatherUrl = `https://api.darksky.net/forecast/${darkSkyApiKey}/${lat},${lng}?lang=tr&units=auto`;
+        
+        axios.get(weatherUrl)
+            .then((response) => {console.log(response.data.currently.temperature)})
+            .catch((e) => {console.error(e)});
+    })
+    .catch((e) => {
+        if (e.code === 'ENOTFOUND') console.error('Unable to connect to API servers!');
+        else console.error(e.message);
+    });
 
